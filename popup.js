2c589a2495e67878a462e16bf837d5a4774abded
G <igauravsehrawat@gmail.com>
2016-03-16 12:41:25 +0530

    manifest.json, popup.js: Working alpha, needed handle permissions appropriately.

--- popup.js
+++ popup.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 	$('#inject-css').on('click', function(){
-		console.log('clicked');
-		chrome.tabs.query({
-			"currentWindow": true,
-			"active": true
-		}, function(tabs){
-			chrome.tabs.insertCSS(tabs[0],'bootstrap.css');
-			console.log('from inside the callback of insertCSS');
-		})
+		chrome.tabs.insertCSS({"file":"bootstrap.css"});
 	});
-})
+});
