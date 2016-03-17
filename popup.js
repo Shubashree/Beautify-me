0c917bf968dbd044f51168870da547faaa3481db
G <igauravsehrawat@gmail.com>
2016-03-17 09:58:43 +0530

    popup.html, popup.js: Modifications relevant to organized external libs. Added independent bootstrap for popup.

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$('#inject-css').on('click', function(){
-		chrome.tabs.insertCSS({"file":"bootstrap.css"});
+		chrome.tabs.insertCSS({"file":"external/bootstrap.css"}, function(){
+			window.close();
+		});
 	});
 });
